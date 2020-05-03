06d2147c01623744864d887b4f5328ff78853366
breytner <productionsvab@gmail.com>
2020-05-03 10:04:26 -0500

    Mi primera pagina web

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+let imagenes = document.querySelectorAll('.gallery_img');
+let modal = document.querySelector('#modal');
+let imagen = document.querySelector('#modal_img');
+let boton = document.querySelector('#modal_boton');
+
+
+for (i=0; i<imagenes.length; i++){
+
+   imagenes[i].addEventListener('click', function(e) {
+       modal.classList.toggle('modal-open');
+        let src = e.target.src;
+        imagen.setAttribute("src", src);
+     
+
+    });
+}
+
+    boton.addEventListener('click', function(){
+        modal.classList.toggle('modal-open');
+    });
+
+
